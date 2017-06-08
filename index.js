22719d4b36beeb53e5f566cc8fb0a789b0eca913
maxgrossman <m.grossman1813@gmail.com>
2017-06-08 17:05:50 -0400

    make initial proxy server
    proxy is hitting the google calendar server and returning the ics data desired...
    but in the hapi handler function it is thinking its an error.
    or maybe there is an error unrelated to the data being transfered...

---
+++ index.js
@@ -0,0 +1,51 @@
+var Hapi = require('hapi');
+// var url = require('url');
+var Wreck = require('wreck');
+
+var server = new Hapi.Server();
+
+server.connection({
+  port: process.env.PORT || 3000
+});
+
+var getGoogleCalendar = function (calendar, next) {
+  const icalUrl = 'https://calendar.google.com/calendar/ical/' + calendar + '/public/basic.ics';
+  console.log(icalUrl);
+  Wreck.get(icalUrl, (err, res, calEvents) => {
+    if (err) {
+      return next(err);
+    }
+    if (res.statusCode !== 200) {
+      return next(res.statusCode);
+    }
+    next(calEvents.toString());
+  });
+};
+
+server.method({
+  name: 'getGoogleCalendar',
+  method: getGoogleCalendar,
+  options: {}
+});
+
+server.route({
+  method: 'GET',
+  path: '/{calendar}/events',
+  handler: function (req, rep) {
+    return server.methods.getGoogleCalendar(req.params.calendar, function (error, result) {
+      if (error) {
+        return rep({
+          error: error
+        }).code(500);
+      }
+      return rep(result);
+    });
+  },
+  config: {
+    cors: true
+  }
+});
+
+server.start(function () {
+  console.log('Starting server at: ' + server.info.uri);
+});
