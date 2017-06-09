9784d17be74e6238741b519d9b73593d2d88a160
maxgrossman <m.grossman1813@gmail.com>
2017-06-09 10:57:23 -0400

    return parsed jcal w/o recurring events
    response now returns timezone and details for non recurring events
    next will handle events that reoccur

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Hapi = require('hapi');
 var Wreck = require('wreck');
 var ical = require('ical');
+// var timezone = require('timezone-js');
 
 var server = new Hapi.Server();
 
@@ -8,10 +9,9 @@ server.connection({
   port: process.env.PORT || 3000
 });
 
-var getGoogleCalendar = function (calendar, next) {
+function getGoogleCalendar (calendar, next) {
   const icalUrl = 'https://calendar.google.com/calendar/ical/' + calendar + '/public/basic.ics';
   console.log(icalUrl);
-  console.log(ical)
   Wreck.get(icalUrl, null, (err, res, calEvents) => {
     if (err) {
       return next(err);
@@ -22,7 +22,37 @@ var getGoogleCalendar = function (calendar, next) {
     // parse ical string to jcal and return.
     next(null, ical.parseICS(calEvents.toString()));
   });
-};
+}
+// deal with the time zone
+  // do we want to make an assumption about what the correct time zone?
+  // or do we just say, from what ever timezone the person is adding in calendar
+  // date info, we set the e time zome for that.
+// format the non repeating examples
+  // we want:
+    // name
+    // location
+    // link to sign up
+// dig into the lovely world of rrule
+function parseJcal (Jcal) {
+  return Object.keys(Jcal).map((key, val) => {
+    if (key.match(/google/)) {
+      if (Jcal[key].rrule) {
+        console.log('it repeats!');
+      } else {
+        console.log('does not repeat');
+        return {
+          'name': Jcal[key].summary,
+          'time': [Jcal[key].start, Jcal[key].end],
+          'location': Jcal[key].location,
+          'description': Jcal[key].description,
+          'link': 'surfing'
+        };
+      }
+    } else {
+      return {'timezone': Jcal[key].tzid};
+    }
+  });
+}
 
 server.method({
   name: 'getGoogleCalendar',
@@ -40,7 +70,8 @@ server.route({
           error: error
         }).code(500);
       }
-      return rep(result);
+      const Jcal = parseJcal(result);
+      return rep(Jcal);
     });
   },
   config: {
