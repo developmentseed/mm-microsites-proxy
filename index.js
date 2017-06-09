d5ed4588fd734314dfdcd25750c4ca7325319f3d
maxgrossman <m.grossman1813@gmail.com>
2017-06-09 13:43:06 -0400

    begin recur helper implementation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Hapi = require('hapi');
 var Wreck = require('wreck');
 var ical = require('ical');
-var rrulestr = require('rrule').rrulestr;
+var recur = require('./recur-helpers/index.js')
 // var timezone = require('timezone-js');
 
 var server = new Hapi.Server();
@@ -30,7 +30,7 @@ function parseJcal (Jcal) {
     if (key.match(/google/)) {
       if (Jcal[key].rrule) {
         console.log('it repeats!');
-        return Jcal[key]
+        return recur(Jcal[key]);
       } else {
         console.log('does not repeat');
         return {
